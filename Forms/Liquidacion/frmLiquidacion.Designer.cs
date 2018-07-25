4f1510b6e773410d38f41175a3bb3a46e853de01
magz1978 <mgomez@redpacifico.com>
2018-07-25 11:22:35 -0600

    Cambios en liquidacion

--- Forms/Liquidacion/frmLiquidacion.Designer.cs
+++ Forms/Liquidacion/frmLiquidacion.Designer.cs
@@ -29,16 +29,31 @@
         private void InitializeComponent()
         {
             this.spCatGrupoFacturaDS1 = new RPSuite.Datasets.Catalogos.spCatGrupoFacturaDS();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
             ((System.ComponentModel.ISupportInitialize)(this.tbControl)).BeginInit();
             this.tbControl.SuspendLayout();
+            this.tpDatos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.vpValidador)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spCatGrupoFacturaDS1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tpDatos
+            // 
+            this.tpDatos.Controls.Add(this.groupControl1);
             // 
             // spCatGrupoFacturaDS1
             // 
             this.spCatGrupoFacturaDS1.DataSetName = "spCatGrupoFacturaDS";
             this.spCatGrupoFacturaDS1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Location = new System.Drawing.Point(125, 29);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(382, 100);
+            this.groupControl1.TabIndex = 0;
+            this.groupControl1.Text = "groupControl1";
             // 
             // frmLiquidacion
             // 
@@ -47,8 +62,10 @@
             this.Name = "frmLiquidacion";
             ((System.ComponentModel.ISupportInitialize)(this.tbControl)).EndInit();
             this.tbControl.ResumeLayout(false);
+            this.tpDatos.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.vpValidador)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spCatGrupoFacturaDS1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +73,6 @@
         #endregion
 
         private Datasets.Catalogos.spCatGrupoFacturaDS spCatGrupoFacturaDS1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
     }
 }
