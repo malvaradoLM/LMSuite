7bb6040538b6f0c0000ef7a8953ead21779376e4
magz1978 <mgomez@redpacifico.com>
2018-07-25 11:42:44 -0600

    ff

--- Forms/Liquidacion/frmLiquidacion.Designer.cs
+++ Forms/Liquidacion/frmLiquidacion.Designer.cs
@@ -30,12 +30,14 @@
         {
             this.spCatGrupoFacturaDS1 = new RPSuite.Datasets.Catalogos.spCatGrupoFacturaDS();
             this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
             ((System.ComponentModel.ISupportInitialize)(this.tbControl)).BeginInit();
             this.tbControl.SuspendLayout();
             this.tpDatos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.vpValidador)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.spCatGrupoFacturaDS1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tpDatos
@@ -49,11 +51,20 @@
             // 
             // groupControl1
             // 
+            this.groupControl1.Controls.Add(this.simpleButton1);
             this.groupControl1.Location = new System.Drawing.Point(125, 29);
             this.groupControl1.Name = "groupControl1";
             this.groupControl1.Size = new System.Drawing.Size(382, 100);
             this.groupControl1.TabIndex = 0;
             this.groupControl1.Text = "groupControl1";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(210, 46);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "simpleButton1";
             // 
             // frmLiquidacion
             // 
@@ -66,6 +77,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.vpValidador)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.spCatGrupoFacturaDS1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -74,5 +86,6 @@
 
         private Datasets.Catalogos.spCatGrupoFacturaDS spCatGrupoFacturaDS1;
         private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
     }
 }
